862cb718189638cc92eba8bd80fce847f99ed5c4
timejp <tokijh@naver.com>
2017-01-31 11:19:37 +0900

    기본 리스트뷰

--- app/src/main/java/com/timejh/basiclist/ListActivity.java
+++ app/src/main/java/com/timejh/basiclist/ListActivity.java
@@ -2,12 +2,23 @@ package com.timejh.basiclist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    private String[] arrays = {"1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        listView = (ListView) findViewById(R.id.listview);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrays);
+        listView.setAdapter(adapter);
     }
 }
