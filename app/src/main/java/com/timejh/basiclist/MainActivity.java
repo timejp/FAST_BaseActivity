27394c7c6511636f9a011313865c9c699bb16549
timejp <tokijh@naver.com>
2017-01-31 10:47:22 +0900

    각각의 액티비티 설정

--- app/src/main/java/com/timejh/basiclist/MainActivity.java
+++ app/src/main/java/com/timejh/basiclist/MainActivity.java
@@ -1,13 +1,48 @@
 package com.timejh.basiclist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bt_listActivity;
+    private Button bt_listHolderActivity;
+    private Button bt_recyclerActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_listActivity = (Button) findViewById(R.id.bt_listActivity);
+        bt_listHolderActivity = (Button) findViewById(R.id.bt_listHolderActivity);
+        bt_recyclerActivity = (Button) findViewById(R.id.br_recyclerActivity);
+
+        bt_listActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bt_listHolderActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListHolderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bt_recyclerActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RecyclerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/timejh/basiclist/RecyclerActivity.java
@@ -0,0 +1,13 @@
+package com.timejh.basiclist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecyclerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle);
+    }
+}
